4178af6b3025d478331b01e5cfbb1cf34e50a149
tannueng <tannueng@gmail.com>
2019-11-14 21:13:55 +0700

    add db name

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 // app.js
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // First you need to create a connection to the db
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'apicusense',
-  password: 'cuapi123',
+  host: "localhost",
+  user: "apicusense",
+  password: "cuapi123",
+  database: "apicusense"
 });
 
-con.connect((err) => {
-  if(err){
-    console.log('Error connecting to Db');
+con.connect(err => {
+  if (err) {
+    console.log("Error connecting to Db");
     return;
   }
-  console.log('Connection established');
+  console.log("Connection established");
 });
 
-con.query('SELECT * FROM station', (err,rows) => {
-    if(err) throw err;
-  
-    console.log('Data received from Db:\n');
-    console.log(rows);
-  });
+con.query("SELECT * FROM station", (err, rows) => {
+  if (err) throw err;
 
-con.end((err) => {
+  console.log("Data received from Db:\n");
+  console.log(rows);
+});
+
+con.end(err => {
   // The connection is terminated gracefully
   // Ensures all previously enqueued queries are still
   // before sending a COM_QUIT packet to the MySQL server.
