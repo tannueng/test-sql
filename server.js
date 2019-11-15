08e5897a2d60be78cb93ef492e0d517f720f27d2
tannueng <tannueng@gmail.com>
2019-11-16 00:41:19 +0700

    change to mysql2

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
-// app.js
-const mysql = require("mysql");
+// // app.js
+// const mysql = require("mysql");
 
-// First you need to create a connection to the db
-const con = mysql.createConnection({
+// // First you need to create a connection to the db
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "apicusense",
+//   password: "cuapi123",
+//   database: "apicusense"
+// });
+
+// con.connect(err => {
+//   if (err) {
+//     console.log("Error connecting to Db");
+//     return;
+//   }
+//   console.log("Connection established");
+// });
+
+// con.query("SELECT * FROM station", (err, rows) => {
+//   if (err) throw err;
+
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
+
+// con.end(err => {
+//   // The connection is terminated gracefully
+//   // Ensures all previously enqueued queries are still
+//   // before sending a COM_QUIT packet to the MySQL server.
+// });
+
+// get the client
+const mysql = require("mysql2");
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
   host: "localhost",
-  user: "apicusense",
-  password: "cuapi123",
-  database: "apicusense"
+  user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-con.connect(err => {
-  if (err) {
-    console.log("Error connecting to Db");
-    return;
-  }
-  console.log("Connection established");
-});
-
-con.query("SELECT * FROM station", (err, rows) => {
-  if (err) throw err;
-
-  console.log("Data received from Db:\n");
+pool.query("SELECT * FROM station", function(err, rows, fields) {
+  // Connection is automatically released when query resolves
   console.log(rows);
-});
-
-con.end(err => {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
 });
